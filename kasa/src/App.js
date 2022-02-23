95a27599361ffa5137f9e12693572d51cebf6dc7
MC <Malckira@gmail.com>
2022-02-23 16:28:06 +0100

    add and modif

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'
+//import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Error from "../src/pages/Error.jsx"
+
+import Header from './components/Header'
+import About from './pages/About'
+
+//import "../src/styles/index.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+
+      <Routes>
+
+        <Route exact path="/" element={<Home />}/>
+
+        <Route path="/About" element={<About />}/>
+
+        <Route path="/Error" element={<Error />}/>
+        
+            
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+//<Route element={<Error />}/>
